7f00eaa03538cb786c7b0684e1970cdf80dd8fbd
Kir-Antipov <kp_antipov@mail.ru>
2020-07-09 14:56:02 +0300

    Made some helpful extension methods for Type

---
+++ AspNetCore.Tools/Helpers/TypeHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace AspNetCore.Tools.Helpers
+{
+    internal static class TypeHelper
+    {
+        public static IEnumerable<TAttribute> GetCustomAttributes<TAttribute>(this Type t) => t.GetCustomAttributes(inherit: false).OfType<TAttribute>();
+
+        [return: MaybeNull]
+        public static TAttribute GetCustomAttribute<TAttribute>(this Type t) => t.GetCustomAttributes<TAttribute>().FirstOrDefault();
+
+        public static bool CouldBeInstance(this Type t) => (t.IsClass || t.IsValueType) && !t.IsAbstract && !t.IsGenericType;
+
+        public static Type? FindImplementation(this Type t)
+        {
+            if (t.CouldBeInstance())
+                return t;
+
+            return ReflectionHelper.GetTypes().FirstOrDefault(x => t.IsAssignableFrom(x) && x.CouldBeInstance());
+        }
+    }
+}
