b8f77a67d57c0486d8ebb53aaa7fa436fa019fbf
Kir-Antipov <kp_antipov@mail.ru>
2020-07-09 14:55:09 +0300

    Made a helper to easily get all available types in the active AppDomain.

---
+++ AspNetCore.Tools/Helpers/ReflectionHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AspNetCore.Tools.Helpers
+{
+    internal static class ReflectionHelper
+    {
+        private static readonly string[] DefaultAssemblies = new[]
+        {
+            "Microsoft",
+            "mscorlib",
+            "netstandard",
+            "Newtonsoft.Json",
+            "System"
+        };
+
+        public static IEnumerable<Assembly> GetAssemblies() => AppDomain.CurrentDomain.GetAssemblies()
+            .Where(x => !x.IsDynamic && !IsDefaultAssembly(x));
+
+        public static IEnumerable<Type> GetTypes() => GetAssemblies().SelectMany(x => { try { return x.GetTypes(); } catch { return Array.Empty<Type>(); } });
+
+        public static IEnumerable<(Type Type, TAttribute Attribute)> GetMarkedTypes<TAttribute>() => GetTypes()
+            .Select(x => (Type: x, Attribute: x.GetCustomAttribute<TAttribute>()))
+            .Where(x => x.Attribute is { });
+
+        private static bool IsDefaultAssembly(Assembly assembly) => string.IsNullOrEmpty(assembly.FullName) || DefaultAssemblies.Any(assembly.FullName.StartsWith);
+    }
+}
