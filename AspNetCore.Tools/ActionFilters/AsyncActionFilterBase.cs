c5bf035b46e3073b9350959b41cdcde498ab727c
Kir-Antipov <kp_antipov@mail.ru>
2020-07-09 12:14:23 +0300

    Added base class for IAsyncActionFilter

---
+++ AspNetCore.Tools/ActionFilters/AsyncActionFilterBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Tools.ActionFilters
+{
+    /// <summary>
+    /// Base class for filters that asynchronously surround execution of the action,
+    /// after model binding is complete.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class AsyncActionFilterBase : Attribute, IAsyncActionFilter
+    {
+        /// <inheritdoc cref="IAsyncActionFilter.OnActionExecutionAsync(ActionExecutingContext, ActionExecutionDelegate)"/>
+        public abstract Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next);
+    }
+}
