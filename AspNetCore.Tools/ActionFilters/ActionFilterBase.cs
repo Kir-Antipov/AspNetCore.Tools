286a5fd577a1aabe354a808b5a4027a3c4c17da9
Kir-Antipov <kp_antipov@mail.ru>
2020-07-09 10:15:52 +0300

    Added base class for IActionFilter.

---
+++ AspNetCore.Tools/ActionFilters/ActionFilterBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace AspNetCore.Tools.ActionFilters
+{
+    /// <summary>
+    /// Base class for filters that surround execution of the action.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class ActionFilterBase : Attribute, IActionFilter
+    {
+        /// <inheritdoc cref="IActionFilter.OnActionExecuting(ActionExecutingContext)"/>
+        public virtual void OnActionExecuting(ActionExecutingContext context) { }
+
+        /// <inheritdoc cref="IActionFilter.OnActionExecuted(ActionExecutedContext)"/>
+        public virtual void OnActionExecuted(ActionExecutedContext context) { }
+    }
+}
