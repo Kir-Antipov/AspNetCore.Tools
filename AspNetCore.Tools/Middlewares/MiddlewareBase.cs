54adb6bd4ccbf065e2fc2256b55c2f40f58fba75
Kir-Antipov <kp_antipov@mail.ru>
2020-07-09 20:34:45 +0300

    Made simple base class for middlewares

---
+++ AspNetCore.Tools/Middlewares/MiddlewareBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Tools.Middlewares
+{
+    /// <summary>
+    /// Base class for simple middlewares.
+    /// </summary>
+    public abstract class MiddlewareBase
+    {
+        /// <summary>
+        /// Invokes next middleware.
+        /// </summary>
+        protected readonly RequestDelegate Next;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MiddlewareBase"/> class.
+        /// </summary>
+        /// <param name="next">Delegate that repersents next middleware action.</param>
+        public MiddlewareBase(RequestDelegate next) => Next = next;
+
+        /// <summary>
+        /// Invokes middleware request processing.
+        /// </summary>
+        /// <param name="context">Encapsulates all HTTP-specific information about current HTTP request.</param>
+        /// <returns>A <see cref="Task"/> that represents the completion of request processing.</returns>
+        public abstract Task Invoke(HttpContext context);
+    }
+}
