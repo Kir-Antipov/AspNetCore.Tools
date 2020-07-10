95f9f049aaa66d609dc3afcc04d0e64ba9f29485
Kir-Antipov <kp_antipov@mail.ru>
2020-07-10 12:32:24 +0300

    Made middleware for enabling XSS protection

---
+++ AspNetCore.Tools/Middlewares/XSSMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Tools.Middlewares
+{
+    /// <summary>
+    /// Middleware which activates the built-in browser protection against xss attacks.
+    /// </summary>
+    public class XSSMiddleware : MiddlewareBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XSSMiddleware"/> class.
+        /// </summary>
+        /// <param name="next">Delegate that repersents next middleware action.</param>
+        public XSSMiddleware(RequestDelegate next) : base(next) { }
+
+        /// <inheritdoc cref="MiddlewareBase.Invoke(HttpContext)"/>
+        public override Task Invoke(HttpContext context)
+        {
+            context.Response.Headers.Add("x-xss-protection", "1");
+            return Next(context);
+        }
+    }
+}
