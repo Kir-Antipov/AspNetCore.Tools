8739dd4be2516991c252c449fc492c6317e170b9
Kir-Antipov <kp_antipov@mail.ru>
2020-07-10 15:43:37 +0300

    Added base class for IModelBinder's

---
+++ AspNetCore.Tools/ModelBinding/ModelBinderBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Tools.ModelBinding
+{
+    /// <summary>
+    /// Base class for model binders.
+    /// </summary>
+    public abstract class ModelBinderBase : IModelBinder
+    {
+        /// <inheritdoc cref="IModelBinder.BindModelAsync(ModelBindingContext)"/>
+        public virtual Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            BindModel(bindingContext);
+            return Task.CompletedTask;
+        }
+
+        /// <returns></returns>
+        /// <inheritdoc cref="IModelBinder.BindModelAsync(ModelBindingContext)"/>
+        public virtual void BindModel(ModelBindingContext bindingContext) { }
+    }
+}
