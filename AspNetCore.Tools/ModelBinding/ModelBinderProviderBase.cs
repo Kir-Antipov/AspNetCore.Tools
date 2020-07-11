53d910ba7a63d8181690c5c9dbfa3c599469e5a8
Kir-Antipov <kp_antipov@mail.ru>
2020-07-11 10:43:39 +0300

    Made base class for IModelBinderProvider

---
+++ AspNetCore.Tools/ModelBinding/ModelBinderProviderBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AspNetCore.Tools.ModelBinding
+{
+    /// <summary>
+    /// Base class for providers of model binders.
+    /// </summary>
+    public abstract class ModelBinderProviderBase : IModelBinderProvider
+    {
+        /// <inheritdoc cref="IModelBinderProvider.GetBinder(ModelBinderProviderContext)"/>
+        public abstract IModelBinder? GetBinder(ModelBinderProviderContext context);
+    }
+}
