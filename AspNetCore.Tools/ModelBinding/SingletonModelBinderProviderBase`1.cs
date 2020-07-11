5551ba5a26a4958c69700d28f2a42b2b25edb06d
Kir-Antipov <kp_antipov@mail.ru>
2020-07-11 15:21:48 +0300

    Made simple ModelBinderProvider for singleton model binders

---
+++ AspNetCore.Tools/ModelBinding/SingletonModelBinderProviderBase`1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace AspNetCore.Tools.ModelBinding
+{
+    /// <summary>
+    /// Base class for providers of model binders that can be automatically created as singletons.
+    /// </summary>
+    /// <typeparam name="TModelBinder">Model binder type.</typeparam>
+    public abstract class SingletonModelBinderProviderBase<TModelBinder> : ModelBinderProviderBase where TModelBinder : IModelBinder, new()
+    {
+        /// <summary>
+        /// Singleton instance of the model binder.
+        /// </summary>
+        public static readonly TModelBinder ModelBinderInstance = new TModelBinder();
+
+        /// <summary>
+        /// This method determines whether the model binder is suitable for the current context or not.
+        /// </summary>
+        /// <param name="context">The <see cref="ModelBinderProviderContext"/>.</param>
+        /// <returns><see langword="true"/> if model binder is suitable; <see langword="false"/> otherwise.</returns>
+        public abstract bool IsSuitable(ModelBinderProviderContext context);
+
+        /// <inheritdoc cref="ModelBinderProviderBase.GetBinder(ModelBinderProviderContext)"/>
+        public override IModelBinder? GetBinder(ModelBinderProviderContext context) => IsSuitable(context) ? ModelBinderInstance : default;
+    }
+}
