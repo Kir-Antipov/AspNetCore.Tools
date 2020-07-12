a8307610df3bcc30fd421449d79f8907bbefd00f
Kir-Antipov <kp_antipov@mail.ru>
2020-07-12 07:50:29 +0300

    Added attribute to mark model binder providers with

---
+++ AspNetCore.Tools/ModelBinding/ModelBinderProviderAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AspNetCore.Tools.ModelBinding
+{
+    /// <summary>
+    /// Marks class as a model binder provider that needs to be registered.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    public class ModelBinderProviderAttribute : Attribute
+    {
+        /// <summary>
+        /// The priority of this model binder provider against others
+        /// (the higher the priority, the earlier model binder provider will be launched).
+        /// </summary>
+        public int Priority { get; set; }
+    }
+}
