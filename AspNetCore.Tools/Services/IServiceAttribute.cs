93372c0ad8253e7cc2a8e87de4e6ba29e0a029fb
Kir-Antipov <kp_antipov@mail.ru>
2020-07-12 12:22:37 +0300

    Added interface for service attributes

---
+++ AspNetCore.Tools/Services/IServiceAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AspNetCore.Tools.Services
+{
+    /// <summary>
+    /// Basic interface for service attributes.
+    /// </summary>
+    public interface IServiceAttribute
+    {
+        /// <summary>
+        /// The <see cref="ServiceLifetime"/> of the service.
+        /// </summary>
+        ServiceLifetime Lifetime { get; }
+
+        /// <summary>
+        /// The <see cref="Type"/> of the service.
+        /// </summary>
+        Type? ServiceType { get; }
+
+        /// <summary>
+        /// A factory used for creating service instances.
+        /// </summary>
+        Type? Factory { get; }
+
+        /// <summary>
+        /// Indicates whether to implement a factory for the service automatically.
+        /// </summary>
+        bool UseAutoImplementedFactory { get; }
+    }
+}
