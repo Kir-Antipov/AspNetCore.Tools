25a21a7c590171012ccfbec6ee8ef9874b6ddff2
Kir-Antipov <kp_antipov@mail.ru>
2020-07-12 13:16:33 +0300

    Made attribute to mark properties that need services with

---
+++ AspNetCore.Tools/Services/InjectServiceAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace AspNetCore.Tools.Services
+{
+    /// <summary>
+    /// Indicates that property needs a service to be injected.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public class InjectServiceAttribute : Attribute { }
+}
