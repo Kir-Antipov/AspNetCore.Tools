3cc3fe6419f49beb2071adff72ca27f512243c1f
Kir-Antipov <kp_antipov@mail.ru>
2020-07-13 19:32:20 +0300

    Made interface for service factories

---
+++ AspNetCore.Tools/Services/IServiceFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AspNetCore.Tools.Services
+{
+    /// <summary>
+    /// Defines a factory for some service.
+    /// </summary>
+    public interface IServiceFactory
+    {
+        /// <summary>
+        /// Provides service.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceProvider"/> instance.</param>
+        /// <returns>Service instance.</returns>
+        object GetService(IServiceProvider services);
+    }
+}
