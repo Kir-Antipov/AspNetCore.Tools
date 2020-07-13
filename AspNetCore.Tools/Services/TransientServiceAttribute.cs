3ce04dcc0298b9b6f536723e6d54f070077b61d2
Kir-Antipov <kp_antipov@mail.ru>
2020-07-13 17:33:58 +0300

    Added attribute for transient services

---
+++ AspNetCore.Tools/Services/TransientServiceAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AspNetCore.Tools.Services
+{
+    /// <summary>
+    /// Marks class as a transient service that needs to be registered.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+    public sealed class TransientServiceAttribute : Attribute, IServiceAttribute
+    {
+        ServiceLifetime IServiceAttribute.Lifetime => ServiceLifetime.Transient;
+
+        bool IServiceAttribute.UseAutoImplementedFactory => false;
+
+        /// <inheritdoc cref="ServiceAttribute.ServiceType"/>
+        public Type? ServiceType { get; set; }
+
+        /// <inheritdoc cref="ServiceAttribute.Factory"/>
+        public Type? Factory { get; set; }
+    }
+}
