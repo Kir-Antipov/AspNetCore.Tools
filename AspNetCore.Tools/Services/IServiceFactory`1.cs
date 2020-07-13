c8d63ad6d4d14000876f6bb77108bea77d9cb66f
Kir-Antipov <kp_antipov@mail.ru>
2020-07-13 19:41:51 +0300

    Made generic version of IServiceFactory

---
+++ AspNetCore.Tools/Services/IServiceFactory`1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AspNetCore.Tools.Services
+{
+    /// <summary>
+    /// Defines a factory for the <typeparamref name="TService"/>.
+    /// </summary>
+    /// <typeparam name="TService">Service type.</typeparam>
+    public interface IServiceFactory<out TService> : IServiceFactory where TService : notnull
+    {
+        object IServiceFactory.GetService(IServiceProvider services) => GetService(services);
+
+        /// <inheritdoc cref="IServiceFactory.GetService(IServiceProvider)"/>
+        new TService GetService(IServiceProvider services);
+    }
+}
