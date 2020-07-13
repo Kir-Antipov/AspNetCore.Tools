c20d45deede159f13a5f3b20cae4532117c72ad0
Kir-Antipov <kp_antipov@mail.ru>
2020-07-13 16:51:15 +0300

    Added attribute for scoped services

---
+++ AspNetCore.Tools/Services/ScopedServiceAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AspNetCore.Tools.Services
+{
+    /// <summary>
+    /// Marks class as a scoped service that needs to be registered.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+    public sealed class ScopedServiceAttribute : Attribute, IServiceAttribute
+    {
+        ServiceLifetime IServiceAttribute.Lifetime => ServiceLifetime.Scoped;
+
+        bool IServiceAttribute.UseAutoImplementedFactory => false;
+
+        /// <inheritdoc cref="ServiceAttribute.ServiceType"/>
+        public Type? ServiceType { get; set; }
+
+        /// <inheritdoc cref="ServiceAttribute.Factory"/>
+        public Type? Factory { get; set; }
+    }
+}
