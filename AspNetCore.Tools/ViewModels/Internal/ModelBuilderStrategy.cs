28c2c50619bbb8c65ca82334290da4bc5418104c
Kir-Antipov <kp_antipov@mail.ru>
2020-07-14 12:13:58 +0300

    Added enum to describe user's ViewModelBuilder strategy

---
+++ AspNetCore.Tools/ViewModels/Internal/ModelBuilderStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AspNetCore.Tools.ViewModels.Internal
+{
+    [Flags]
+    internal enum ModelBuilderStrategy
+    {
+        None,
+        CreateAndBuild          = Create | Build,
+        TryBuild                = Try | Build,
+        CreateAndTryBuild       = Create | TryBuild,
+        BuildAsync              = Build | Async,
+        CreateAndBuildAsync     = Create | BuildAsync,
+        TryBuildAsync           = Try | BuildAsync,
+        CreateAndTryBuildAsync  = Create | TryBuildAsync,
+
+        Build   = 0b0001,
+        Create  = 0b0010,
+        Async   = 0b0100,
+        Try     = 0b1000
+    }
+}
