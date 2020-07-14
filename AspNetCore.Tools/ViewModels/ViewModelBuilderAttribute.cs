1c4a8093cda1ab63d7a14d3c18e1c952389b42f6
Kir-Antipov <kp_antipov@mail.ru>
2020-07-14 10:48:49 +0300

    Made attribute to mark ViewModel builders with

---
+++ AspNetCore.Tools/ViewModels/ViewModelBuilderAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AspNetCore.Tools.ViewModels
+{
+    /// <summary>
+    /// Marks class as a valid ViewModelBuilder to be used.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    public class ViewModelBuilderAttribute : Attribute
+    {
+        /// <summary>
+        /// Model type associated with this ViewModelBuilder.
+        /// </summary>
+        public Type? ViewModelType { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ViewModelBuilderAttribute"/> class.
+        /// </summary>
+        /// <param name="viewModelType">Model type associated with this ViewModelBuilder.</param>
+        public ViewModelBuilderAttribute(Type? viewModelType = null) => ViewModelType = viewModelType;
+    }
+}
