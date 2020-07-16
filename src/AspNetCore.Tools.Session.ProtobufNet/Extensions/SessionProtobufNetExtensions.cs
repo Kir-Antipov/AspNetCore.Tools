7408394762e4ff6bdae43936af978425bcbf893c
Kir-Antipov <kp_antipov@mail.ru>
2020-07-16 19:01:04 +0300

    Implemented protobuf-net serializer

---
+++ src/AspNetCore.Tools.Session.ProtobufNet/Extensions/SessionProtobufNetExtensions.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCore.Tools.Session;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AspNetCore.Tools.Extensions
+{
+    /// <summary>
+    /// Provides a set of static (Shared in Visual Basic) helpful methods for objects
+    /// that implement <see cref="ISession"/>.
+    /// </summary>
+    public static class SessionProtobufNetExtensions
+    {
+        /// <inheritdoc cref="SessionExtensions.Set(ISession, string, int)"/>
+        public static void Set<T>(this ISession session, string key, [DisallowNull] T value) => SessionProtobufNetSerializer<T>.Instance.Set(session, key, value);
+
+        /// <inheritdoc cref="SessionExtensions.GetInt64(ISession, string)"/>
+        [return: MaybeNull]
+        public static T Get<T>(this ISession session, string key) => SessionProtobufNetSerializer<T>.Instance.Get(session, key);
+
+        /// <inheritdoc cref="SessionExtensions.GetRequiredInt64(ISession, string)"/>
+        [return: NotNull]
+        public static T GetRequired<T>(this ISession session, string key) => SessionProtobufNetSerializer<T>.Instance.GetRequired(session, key);
+
+        /// <inheritdoc cref="SessionExtensions.TryGetValue(ISession, string, out string?)"/>
+        public static bool TryGetValue<T>(this ISession session, string key, [MaybeNull, NotNullWhen(true)] out T value) => SessionProtobufNetSerializer<T>.Instance.TryGetValue(session, key, out value);
+    }
+}
---
+++ src/AspNetCore.Tools.Session.ProtobufNet/Session/SessionProtobufNetSerializer`1.cs
@@ -0,0 +1,31 @@
+﻿using ProtoBuf;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace AspNetCore.Tools.Session
+{
+    /// <summary>
+    /// Implementation of the <see cref="SessionValueSerializer{T}"/> for protobuf-net.
+    /// </summary>
+    /// <typeparam name="T">Entity type.</typeparam>
+    public class SessionProtobufNetSerializer<T> : SessionValueSerializer<T>
+    {
+        internal static SessionProtobufNetSerializer<T> Instance = Create<SessionProtobufNetSerializer<T>, T>();
+
+        /// <inheritdoc cref="SessionValueSerializer{T}.Serialize(T)"/>
+        protected override byte[] Serialize([DisallowNull] T value)
+        {
+            using MemoryStream memory = new MemoryStream();
+            Serializer.Serialize(memory, value);
+            return memory.ToArray();
+        }
+
+        /// <inheritdoc cref="SessionValueSerializer{T}.Deserialize(byte[])"/>
+        [return: NotNull]
+        protected override T Deserialize(byte[] data)
+        {
+            using MemoryStream memory = new MemoryStream(data);
+            return Serializer.Deserialize<T>(memory)!;
+        }
+    }
+}
