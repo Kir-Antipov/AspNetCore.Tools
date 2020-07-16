5a699f160cd5864f16b6b663a854788bbdd4a5e0
Kir-Antipov <kp_antipov@mail.ru>
2020-07-16 19:18:19 +0300

    Implemented Newtonsoft.Json serializer

---
+++ src/AspNetCore.Tools.Session.NewtonsoftJson/Extensions/SessionProtobufNetExtensions.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCore.Tools.Session.NewtonsoftJson.Session;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AspNetCore.Tools.Extensions
+{
+    /// <summary>
+    /// Provides a set of static (Shared in Visual Basic) helpful methods for objects
+    /// that implement <see cref="ISession"/>.
+    /// </summary>
+    public static class SessionNewtonsoftJsonExtensions
+    {
+        /// <inheritdoc cref="SessionExtensions.Set(ISession, string, int)"/>
+        public static void Set<T>(this ISession session, string key, [DisallowNull] T value) => SessionNewtonsoftJsonSerializer<T>.Instance.Set(session, key, value);
+
+        /// <inheritdoc cref="SessionExtensions.GetInt64(ISession, string)"/>
+        [return: MaybeNull]
+        public static T Get<T>(this ISession session, string key) => SessionNewtonsoftJsonSerializer<T>.Instance.Get(session, key);
+
+        /// <inheritdoc cref="SessionExtensions.GetRequiredInt64(ISession, string)"/>
+        [return: NotNull]
+        public static T GetRequired<T>(this ISession session, string key) => SessionNewtonsoftJsonSerializer<T>.Instance.GetRequired(session, key);
+
+        /// <inheritdoc cref="SessionExtensions.TryGetValue(ISession, string, out string?)"/>
+        public static bool TryGetValue<T>(this ISession session, string key, [MaybeNull, NotNullWhen(true)] out T value) => SessionNewtonsoftJsonSerializer<T>.Instance.TryGetValue(session, key, out value);
+    }
+}
---
+++ src/AspNetCore.Tools.Session.NewtonsoftJson/Session/SessionNewtonsoftJsonSerializer`1.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace AspNetCore.Tools.Session.NewtonsoftJson.Session
+{
+    /// <summary>
+    /// Implementation of the <see cref="SessionValueSerializer{T}"/> for Newtonsoft.Json.
+    /// </summary>
+    /// <typeparam name="T">Entity type.</typeparam>
+    public class SessionNewtonsoftJsonSerializer<T> : SessionValueSerializer<T>
+    {
+        internal static SessionNewtonsoftJsonSerializer<T> Instance = Create<SessionNewtonsoftJsonSerializer<T>, T>();
+
+        /// <inheritdoc cref="SessionValueSerializer{T}.Serialize(T)"/>
+        protected override byte[] Serialize([DisallowNull] T value) => Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(value));
+
+        /// <inheritdoc cref="SessionValueSerializer{T}.Deserialize(byte[])"/>
+        [return: NotNull]
+        protected override T Deserialize(byte[] data) => JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(data))!;
+    }
+}
