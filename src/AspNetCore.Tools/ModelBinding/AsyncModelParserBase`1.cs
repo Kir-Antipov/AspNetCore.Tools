c31f65f409b3d0694e28b5ed9e292c980b2ac22c
Kir-Antipov <kp_antipov@mail.ru>
2020-07-17 14:45:56 +0300

    Added generic version of the AsyncModelParserBase

---
+++ src/AspNetCore.Tools/ModelBinding/AsyncModelParserBase`1.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace AspNetCore.Tools.ModelBinding
+{
+    /// <summary>
+    /// Generic version of the <see cref="AsyncModelParserBase"/>.
+    /// </summary>
+    /// <typeparam name="TModel">Model type.</typeparam>
+    public abstract class AsyncModelParserBase<TModel> : AsyncModelParserBase
+    {
+        /// <inheritdoc cref="AsyncModelParserBase.TryParseAsync(string)"/>
+        public sealed override async Task<(bool success, object? result)> TryParseAsync(string value) => await TryParseModelAsync(value);
+
+        /// <inheritdoc cref="TryParseAsync(string)"/>
+        public abstract Task<(bool success, TModel result)> TryParseModelAsync(string value);
+    }
+}
