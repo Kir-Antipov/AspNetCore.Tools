a2124b495ee50455b7ff4c2450ea4bcdb6f256fd
Kir-Antipov <kp_antipov@mail.ru>
2020-07-17 14:45:22 +0300

    Added generic version of ViewModelBuilderBase

---
+++ src/AspNetCore.Tools/ViewModels/ViewModelBuilderBase`1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Tools.ViewModels
+{
+    /// <inheritdoc cref="ViewModelBuilderBase"/>
+    public abstract class ViewModelBuilderBase<TController> : ViewModelBuilderBase where TController : ControllerBase
+    {
+        /// <inheritdoc cref="ViewModelBuilderBase.Controller"/>
+        public new TController Controller { get; }
+
+        /// <inheritdoc cref="ViewModelBuilderBase(ControllerBase)"/>
+        public ViewModelBuilderBase(TController controller) : base(controller) => Controller = controller;
+    }
+}
